9af8917b12a65d82057c144c5c950ba701340b9b
amanbhai1 <49727102+amanbhai1@users.noreply.github.com>
2024-06-02 22:36:17 +0530

    Add Key

--- script.js
+++ script.js
@@ -22,6 +22,12 @@ function clearDisplay() {
   display.value = '';
 }
 
+
+
+function Del() {
+  display.value = display.value.toString().slice(0,-1);
+}
+
 function calculate() {
   try {
     display.value = eval(display.value);
